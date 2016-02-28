b1993f829f574b8069665541a444b9cd45039563
Ashley Cutmore <ashley.cutmore@gmail.com>
2016-02-28 13:20:56 +0000

    Avoids DNS lookup when starting worker

--- workers.php
+++ workers.php
@@ -6,7 +6,12 @@ class Workers {
       return $_SERVER['SERVER_PORT'];
     }
 
-    static private function getHost(){
+    static private function getAddress(){
+      return empty($_SERVER['SERVER_ADDR']) ?
+        static::getHTTPHost() : $_SERVER['SERVER_ADDR'];
+    }
+
+    static private function getHTTPHost(){
       return explode(':', $_SERVER['HTTP_HOST'])[0];
     }
 
@@ -14,23 +19,22 @@ class Workers {
       return '/' . dirname($_SERVER['REQUEST_URI']) . '/' . $worker . '.php';
     }
 
+    /**
+    Send a HTTP GET to another php script to run then close connection immediately
+    */
     static public function sendMessage($worker, $data){
-      $data = http_build_query($data);
 
-      $fp = fsockopen(static::getHost(), static::getPort(), $errno, $errstr, 30);
+      $data = http_build_query($data);
+      $fp = fsockopen(static::getAddress(), static::getPort(), $errno, $errstr, 10);
+
       if (!$fp) {
           echo "$errstr ($errno)<br />\n";
       } else {
           $resource_uri = static::getWorker($worker) . '?' . $data;
           $out = "GET " . $resource_uri . " HTTP/1.1\r\n";
-          $out .= "Host: " . static::getHost() . "\r\n";
+          $out .= "Host: " . static::getHTTPHost() . "\r\n";
           $out .= "Connection: Close\r\n\r\n";
           fwrite($fp, $out);
-          /*
-          while (!feof($fp)) {
-              echo fgets($fp, 128);
-          }
-          */
           fclose($fp);
       }
 
