572d06e7b5b192b73eef9f52c504aeed6e99f0c8
Ashley Cutmore <ashley.cutmore@gmail.com>
2016-03-06 19:20:49 +0000

    Tidied up CONFIG example

--- CONFIG.php
+++ CONFIG.php
@@ -2,19 +2,16 @@
 
 // Configuration settings listed by slack token
 
+$bb_account_a = [
+  "bb_client_id" => "kjihgfedcba",
+  "bb_client_secret" => "123456abcdef",
+  "bb_account" => "foo",
+  "bb_repo" => "bar"
+];
+
 $bbi_slack_config = [
-    "abcdefghijklmnopqrs" => [
-      "bb_client_id" => "kjihgfedcba",
-      "bb_client_secret" => "123456abcdef",
-      "bb_account" => "foo",
-      "bb_repo" => "bar",
-    ],
-    "srqponmlkjihgfedcba" => [
-      "bb_client_id" => "kjihgfedcba",
-      "bb_client_secret" => "123456abcdef",
-      "bb_account" => "foo",
-      "bb_repo" => "rar",
-    ],
+    "abcdefghijklmnopqrs" => $bb_account_a,
+    "srqponmlkjihgfedcba" => $bb_account_a,
 ];
 
 include 'CONFIG_.php'; // dev enviroment overrides
